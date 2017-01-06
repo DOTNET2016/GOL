505ef664face26681980bf08dba84291c1cf4e03
LappOlle <skogsmulle87@gmail.com>
2017-01-06 01:17:53 +0100

    Setted the timer interval to 1 seconds.

--- GOL/GOLHandler.cs
+++ GOL/GOLHandler.cs
@@ -23,7 +23,7 @@ namespace GOL
         public GOLHandler()
         {
             timer = new DispatcherTimer();
-            timer.Interval = (new TimeSpan(600));
+            timer.Interval = (new TimeSpan(0,0,1));// Interval set to one second.
             timer.IsEnabled = true;
             timer.Stop();
             timer.Tick += Timer_Tick;
