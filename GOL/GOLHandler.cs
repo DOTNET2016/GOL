eb3fb249cee3ba69d28e384d013f051bedba9485
FiveEYZ <b-d-t@msn.com>
2017-01-16 10:44:50 +0100

    code cleanup

--- GOL/GOLHandler.cs
+++ GOL/GOLHandler.cs
@@ -30,11 +30,6 @@ namespace GOL
             ActualGeneration.SetValue(cell, cell.X, cell.Y);
         }
 
-        public Cell[,] GetActualGeneration()
-        {
-            return ActualGeneration;
-        }
-
         public void CalculateNextGeneration()
         {
             for (int i = 0; i < ActualGeneration.GetLength(0); i++)
@@ -210,7 +205,6 @@ namespace GOL
             using (GContext db = new GContext())
             {
                 db.SavedGames.Add(savedGame);
-
                 foreach (var item in AliveCells)
                 {
                     Generation g = new Generation();
@@ -219,7 +213,6 @@ namespace GOL
                     g.Cell_X = item.X;
                     g.Cell_Y = item.Y;
                     db.Generation.Add(g);
-
                 }
                 db.SaveChanges();
                 AliveCells.Clear();
@@ -234,12 +227,9 @@ namespace GOL
                 context.Generation.RemoveRange(from g in context.Generation
                                                where g.SavedGame_id == savedGame
                                                select g);
-
-
                 context.SavedGames.Remove(savedGameToDelete);
                 context.SaveChanges();
             }
-
         }
 
         public List<Generation> LoadGenFromDatabase()
@@ -248,7 +238,6 @@ namespace GOL
             using (GContext db = new GContext())
             {
                 var currentGen = db.Generation.Where(x => x.SavedGames.Player_id == activePlayer.id);
-
                 try
                 {
                     foreach (var gen in currentGen)
@@ -258,14 +247,10 @@ namespace GOL
                 }
                 catch (Exception)
                 {
-
                     MessageBox.Show("That didn't work", "Warning!");
                 }
-
             }
             return generationsToReturn;
-        }
-
-        
+        } 
     }
 }
